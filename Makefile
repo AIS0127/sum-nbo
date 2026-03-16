04b850325ebf6abb2d8e9344e8092404aef7e390
AIS0127 <y0un9sa@gmail.com>
2026-03-16 20:19:10 +0900

    add main.c Makefile

---
+++ Makefile
@@ -0,0 +1,12 @@
+#Makefile
+all: sum-nbo
+
+sum-nbo: main.o 
+	g++ -o sum-nbo main.o 
+
+main.o: main.c
+	g++ -c -o main.o main.c
+
+clean:
+	rm -f sum-nbo
+	rm -f *.o
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main(unsigned int argc, char * argv[]){
+    if( argc < 2 ){
+        printf("syntax : sum-nbo <file1> [<file2>...] ");
+    }
+
+    
+}
